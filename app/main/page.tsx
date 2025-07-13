814aaf69b64b8ca7ba11558b67fed6b81e4f5f39
seif2003 <seifddinebenamara2022@gmail.com>
2025-07-13 19:17:32 +0100

    Refactor navigation and update routes to replace '/protected' with '/main'; add MainPage component for virtual tour access

---
+++ app/main/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import VirtualTour from "@/components/virtual-tour";
+
+export default function MainPage() {
+    // Allow both authenticated users and guests to access the virtual tour
+    return (
+        <div className="w-full">
+            {/* Virtual Tour - accessible to both authenticated users and guests */}
+            <VirtualTour height="100vh" width="100%" startingScene="a1" showNavbar={true} accountForNavbar={true} startingPitch={0} startingYaw={-1.57} />
+        </div>
+    );
+}
