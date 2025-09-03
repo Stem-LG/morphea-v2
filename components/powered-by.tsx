882e9a5c8c88e112e67ab23afc8962c8f0f8a1c2
Louay Ghanney <contact@louay.tn>
2025-09-03 13:19:59 +0100

    minify powered by

--- components/powered-by.tsx
+++ components/powered-by.tsx
@@ -21,8 +21,8 @@ export function PoweredBy() {
     }
 
     return (
-        <div className="fixed bottom-0 justify-end z-20 pr-6 mb-2 flex w-full">
-            <div className="rounded-lg border scale-125 border-white/10 bg-black/40 px-3 py-2">
+        <div className="fixed bottom-0 justify-end z-20 pr-2 mb-2 flex w-full">
+            <div className="rounded-lg border border-white/10 bg-black/40 px-2 py-1">
                 {mellimeUrl && !isLoadingMellimeUrl ? (
                     <button
                         onClick={handleClick}
