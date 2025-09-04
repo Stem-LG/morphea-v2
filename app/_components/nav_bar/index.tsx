58eee13d649091949785a1f9ee2df126c532de32
seif2003 <seifddinebenamara2022@gmail.com>
2025-09-04 14:13:11 +0100

    fix: adjust height of navbar and sidebar for improved layout

--- app/_components/nav_bar/index.tsx
+++ app/_components/nav_bar/index.tsx
@@ -272,7 +272,7 @@ export default function NavBar() {
                 setIsWishlistOpen={setIsWishlistOpen}
             />
 
-            <div className="h-24 bg-white" />
+            <div className="h-18 bg-white" />
         </>
     )
 }
