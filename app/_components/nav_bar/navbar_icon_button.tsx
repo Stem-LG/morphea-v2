65e1d25e2408ec3f2a9dea6cf4673599833031ed
Louay Ghanney <contact@louay.tn>
2025-09-04 08:38:11 +0100

    update cart icon

--- app/_components/nav_bar/navbar_icon_button.tsx
+++ app/_components/nav_bar/navbar_icon_button.tsx
@@ -13,7 +13,7 @@ export function NavBarIconButton({
         <div
             onClick={onClick}
             className={cn(
-                'stroke-morpheus-blue-dark flex cursor-pointer items-center justify-center transition duration-300 hover:stroke-white',
+                'fill-morpheus-blue-dark stroke-morpheus-blue-dark flex cursor-pointer items-center justify-center transition duration-300 hover:stroke-white hover:fill-white',
                 variant === 'leading' ? 'size-8 md:size-9' : 'size-10'
             )}
         >
