ac1f90701e9052788f0e28a4e169194fe26b2284
Louay Ghanney <contact@louay.tn>
2025-09-04 20:55:02 +0100

    fix cart icon

--- app/main/_components/product-details-page.tsx
+++ app/main/_components/product-details-page.tsx
@@ -13,6 +13,7 @@ import { useRemoveFromWishlist } from '@/app/_hooks/wishlist/useRemoveFromWishli
 import { useIsInWishlist } from '@/app/_hooks/wishlist/useIsInWishlist'
 import * as THREE from 'three'
 import { StarIcon } from 'lucide-react'
+import { CartIcon } from '@/app/_icons/cart_icon'
 
 // Loading component for 3D model
 function LoadingSpinner() {
@@ -969,23 +970,11 @@ export function ProductDetailsPage({
                                                 : ''
                                         }`}
                                     >
-                                        <span className="flex items-center justify-center">
+                                        <span className="flex items-center justify-center gap-2 fill-white">
                                             {addToCartMutation.isPending ? (
                                                 <div className="mr-2 h-5 w-5 animate-spin rounded-full border-2 border-white border-t-transparent"></div>
                                             ) : (
-                                                <svg
-                                                    className="mr-2 h-5 w-5"
-                                                    fill="none"
-                                                    stroke="currentColor"
-                                                    viewBox="0 0 24 24"
-                                                >
-                                                    <path
-                                                        strokeLinecap="round"
-                                                        strokeLinejoin="round"
-                                                        strokeWidth={2}
-                                                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                                                    />
-                                                </svg>
+                                               <CartIcon/>
                                             )}
                                             {addToCartMutation.isPending
                                                 ? 'Adding...'
